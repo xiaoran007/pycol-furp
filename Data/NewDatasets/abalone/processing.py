5791ef14cfc5ec593c66b2321f9c60cd5a9597f9
xiaoran <xiaoran2495897824@163.com>
2024-04-25 11:38:16 +0800

    update: add abalone

---
+++ Data/NewDatasets/abalone/processing.py
@@ -0,0 +1,24 @@
+import pandas as pd
+
+"""
+    imbalance ratio minority class : majority class = 364: 3810 (Total: 4174)
+    minority class: "Rings" >= 15
+    majority class: "Rings" < 15
+    meta: [1, 0, 0, 0, 0, 0, 0, 0]
+"""
+
+
+def main():
+    header = ["Sex", "Length", "Diameter", "Height", "Whole_weight", "Shucked_weight", "Viscera_weight", "Shell_weight",
+              "Rings"]
+    df = pd.read_csv("./abalone_rmHead.dat", names=header)
+    df.loc[df['Rings'] >= 15, 'class'] = 1
+    df.loc[df['Rings'] < 15, 'class'] = 0
+    df = df.drop(["Rings"], axis=1)
+    print(df)
+    print(df["class"].value_counts())
+    df.to_csv("./abalone.csv", index=False)
+
+
+if __name__ == "__main__":
+    main()
---
+++ Data/NewDatasets/processedDataset.py
@@ -0,0 +1,10 @@
+class ProcessedDataset(object):
+    def __init__(self):
+        self.datasetNameList = list()
+        self.datasetNameList.append("abalone")
+
+    def GetDatasetList(self):
+        return self.datasetNameList
+
+
+
