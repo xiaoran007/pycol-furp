d63060173f37744e21e3940b7efb9a2c4ad4bce3
xiaoran <xiaoran2495897824@163.com>
2024-04-27 14:47:55 +0800

    update: process company bankruptcy

---
+++ Data/NewDatasets/Company_Bankruptcy/processing.py
@@ -0,0 +1,33 @@
+import pandas as pd
+from sklearn.preprocessing import LabelEncoder
+
+"""
+    imbalance ratio minority class : majority class = 220 : 6599 (Total: 6819)
+    minority class: "Bankrupt?" == "1"
+    majority class: "Bankrupt?" == "0"
+    meta: 0-92: 0, 93-94: 1
+"""
+
+
+def main():
+    df = pd.read_csv("./data.csv", delimiter=',')
+    print(df)
+    print(df["Bankrupt?"].value_counts())
+    df = df.dropna(axis=1)
+    print(df["Bankrupt?"].value_counts())
+
+    df[" Liability-Assets Flag-C"] = df[" Liability-Assets Flag"]
+    df[" Net Income Flag-C"] = df[" Net Income Flag"]
+
+    df.loc[df['Bankrupt?'] == 1, 'class'] = 1
+    df.loc[df['Bankrupt?'] == 0, 'class'] = 0
+
+    print(df["class"].value_counts())
+    df = df.drop(["Bankrupt?"], axis=1)
+    df = df.drop([" Liability-Assets Flag"], axis=1)
+    df = df.drop([" Net Income Flag"], axis=1)
+    df.to_csv("./Company_Bankruptcy.csv", index=False)
+
+
+if __name__ == "__main__":
+    main()
--- Data/NewDatasets/processedDataset.py
+++ Data/NewDatasets/processedDataset.py
@@ -1,15 +1,44 @@
 class ProcessedDataset(object):
     def __init__(self):
         self.datasetNameList = list()
+        self.datasetMetaDict = dict()
+        self._init_datasetNameList()
+        self._init_datasetMetaDict()
+
+    def GetDatasetList(self):
+        return self.datasetNameList
+
+    def GetDatasetMeta(self, dataset_name):
+        if dataset_name not in self.datasetNameList:
+            print('Err.')
+        else:
+            return self.datasetMetaDict[dataset_name]
+
+
+    def _init_datasetNameList(self):
         self.datasetNameList.append("abalone")
         self.datasetNameList.append("adult")
         self.datasetNameList.append("balance")
         self.datasetNameList.append("breast")
         self.datasetNameList.append("car")
         self.datasetNameList.append("cleveland")
+        self.datasetNameList.append("Company_Bankruptcy")
 
-    def GetDatasetList(self):
-        return self.datasetNameList
+    def _init_datasetMetaDict(self):
+        self.datasetMetaDict["abalone"] = [1, 0, 0, 0, 0, 0, 0, 0]
+        self.datasetMetaDict["adult"] = [0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 0, 0, 0, 1]
+        self.datasetMetaDict["balance"] = [0, 0, 0, 0]
+        self.datasetMetaDict["breast"] = [1, 1, 1, 1, 1, 1, 1, 1, 1]
+        self.datasetMetaDict["car"] = [1, 1, 1, 1, 1, 1]
+        self.datasetMetaDict["cleveland"] = [0, 1, 1, 0, 0, 1, 1, 0, 1, 0, 1, 1, 1]
+        self.datasetMetaDict["Company_Bankruptcy"] = self._Company_Bankruptcy()
 
-
+    @staticmethod
+    def _Company_Bankruptcy():
+        meta = list()
+        for i in range(93):
+            meta.append(0)
+        meta.append(1)
+        meta.append(1)
+        return meta
 
