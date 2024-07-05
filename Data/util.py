ccb6e148f2970970a436f9450dee9477f48b6ce8
xiaoran <xiaoran2495897824@163.com>
2024-07-05 15:59:03 +0800

    update: util.py

--- Data/util.py
+++ Data/util.py
@@ -3,6 +3,8 @@ import pandas as pd
 from arff import dump
 from arff import loads
 from Data.DatasetsLoader import Dataset
+from Data.NewDatasets.processedDataset import ProcessedDataset
+from Data.NewDatasetsLoader import Dataset as newDataset
 
 
 class Util(object):
@@ -14,32 +16,14 @@ class Util(object):
         with open(filename, 'w') as f:
             dump(data, f)
 
-    def csv2arff(self, csv_filename, arff_filename):
-        pass
-
-
-datasets_list = ['Africa', 'BankNote', 'CreditApproval', 'CreditRisk', 'CreditCard', 'Ecoli', 'FakeBills',
-                 'LoanPrediction', 'PageBlock', 'PageBlockDel', 'PredictTerm', 'SouthGermanCredit', 'Wine', 'WineRed',
-                 'Yeast', 'YeastUn']
-
-# Define the base directory where datasets are stored
-base_directory = "Datasets"
-
-
-def save_as_arff(data, filename):
-    with open(filename, 'w') as f:
-        dump(data, f)
-
-
-def load_and_save_datasets():
-    for dataset_name in datasets_list:
+    def csv2arff(self, dataset_name, cwd):
+        os.chdir(os.path.dirname(__file__))
         print(f"Processing {dataset_name} dataset...")
-        dataset = Dataset(dataset_name)
+        dataset = newDataset(dataset_name, os.path.dirname(__file__))
         X, y = dataset.GetDataset()
         result = pd.concat([pd.DataFrame(X), pd.Series(y, name="target")], axis=1)
-        csv_filename = os.path.join('./EncodeDatasetsNew', f'{dataset_name}_result.csv')
-        result.to_csv(csv_filename, index=False)  # Export to CSV
-        # Save as ARFF (you can use your previously defined method to save as ARFF)
+        csv_filename = os.path.join('./NewDatasetsEncode', f'{dataset_name}_result.csv')
+        result.to_csv(csv_filename, index=False)
 
         arff_data = {
             'description': '',
@@ -48,10 +32,11 @@ def load_and_save_datasets():
             'data': result.values.tolist()
         }
 
-        # Save as ARFF
-        arff_filename = os.path.join('./EncodeDatasetsNew', f'{dataset_name}.arff')
-        save_as_arff(arff_data, arff_filename)
+        arff_filename = os.path.join('./NewDatasetsEncode', f'{dataset_name}.arff')
+        self.save_as_arff(arff_data, arff_filename)
+        os.chdir(cwd)
+        print(f"Processed {dataset_name} dataset.")
 
 
-if __name__ == "__main__":
-    load_and_save_datasets()
+
+
