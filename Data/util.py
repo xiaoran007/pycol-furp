e7f7cff9147e382cfcc6f97b24024169fd3e389c
xiaoran <xiaoran2495897824@163.com>
2024-07-06 20:25:58 +0800

    update: arff encode

--- Data/util.py
+++ Data/util.py
@@ -8,8 +8,15 @@ from Data.NewDatasetsLoader import Dataset as newDataset
 
 
 class Util(object):
-    def __init__(self):
-        pass
+    def __init__(self, dataset_type):
+        self.type = dataset_type
+        self.path = self._setPath()
+
+    def _setPath(self):
+        if self.type == "new":
+            return "./NewDatasetsEncode"
+        elif self.type == "old":
+            return "./DatasetsEncode"
 
     @staticmethod
     def save_as_arff(data, filename):
@@ -19,10 +26,20 @@ class Util(object):
     def csv2arff(self, dataset_name, cwd):
         os.chdir(os.path.dirname(__file__))
         print(f"Processing {dataset_name} dataset...")
-        dataset = newDataset(dataset_name, os.path.dirname(__file__))
+        if self.type == "new":
+            dataset = newDataset(dataset_name, os.path.dirname(__file__))
+        elif self.type == "old":
+            dataset = Dataset(dataset_name)
+        else:
+            print("Err.")
+            exit(1)
         X, y = dataset.GetDataset()
         result = pd.concat([pd.DataFrame(X), pd.Series(y, name="target")], axis=1)
-        csv_filename = os.path.join('./NewDatasetsEncode', f'{dataset_name}_result.csv')
+        csv_filename = os.path.join(self.path, f'{dataset_name}_result.csv')
+        if os.path.exists(self.path):
+            pass
+        else:
+            os.mkdir(self.path)
         result.to_csv(csv_filename, index=False)
 
         arff_data = {
@@ -32,7 +49,7 @@ class Util(object):
             'data': result.values.tolist()
         }
 
-        arff_filename = os.path.join('./NewDatasetsEncode', f'{dataset_name}.arff')
+        arff_filename = os.path.join(self.path, f'{dataset_name}.arff')
         self.save_as_arff(arff_data, arff_filename)
         os.chdir(cwd)
         print(f"Processed {dataset_name} dataset.")
--- encode.py
+++ encode.py
@@ -1,11 +1,12 @@
 import os
 from Data.NewDatasets.processedDataset import ProcessedDataset
-from Data.NewDatasetsLoader import Dataset
+from Data.NewDatasetsLoader import Dataset as newDataset
+from Data.DatasetsLoader import Datasets_list, Dataset
 from Data import util
 
 
-dataset_list = ProcessedDataset().datasetNameList
-helper = util.Util()
+dataset_list = Datasets_list
+helper = util.Util(dataset_type="old")
 
 for dataset_name in dataset_list:
     helper.csv2arff(dataset_name, os.path.join(os.path.dirname(__file__)))
